edf0bf3f32dea43279daf7de9382e0b2f931f25c
Jan Schmidt <hi@jnsmdt.dev>
2024-05-31 13:06:58 +0200

    changed path to openscad nightly

--- gen_samples.py
+++ gen_samples.py
@@ -5,33 +5,44 @@ import csv
 import os
 import platform
 
-if platform.system() == 'Windows':
-    OPENSCAD = 'C:\Program Files\OpenSCAD\openscad.exe'
-elif platform.system() == 'Linux':
-     OPENSCAD = 'openscad'
+if platform.system() == "Windows":
+    OPENSCAD = "C:\Program Files\OpenSCAD (Nightly)\openscad.exe"
+elif platform.system() == "Linux":
+    OPENSCAD = "openscad"
 else:
-    OPENSCAD = '/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD'
+    OPENSCAD = "/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD"
 
-MYDIR=os.path.dirname(os.path.realpath(__file__))
+MYDIR = os.path.dirname(os.path.realpath(__file__))
+
 
 def gen_samples(file=f"{MYDIR}/samples.csv"):
     os.makedirs(f"{MYDIR}/stl", exist_ok=True)
-    with open(file, '+r') as f:
+    with open(file, "+r") as f:
         data = csv.reader(f)
 
         for l in data:
             print("Processing:", l)
             filename = "stl/" + "_".join(l) + ".stl"
             subprocess.run(
-                [OPENSCAD,
-                '-o', filename,
-                '-D', f'BRAND="{l[0]}"',
-                '-D', f'TYPE="{l[1]}"',
-                '-D', f'COLOR="{l[2]}"',
-                '-D', f'TEMP_HOTEND="{l[3]}"',
-                '-D', f'TEMP_BED="{l[4]}"',
-                f'{MYDIR}/FilamentSamples.scad',
-                ], check=True)
+                [
+                    OPENSCAD,
+                    "-o",
+                    filename,
+                    "-D",
+                    f'BRAND="{l[0]}"',
+                    "-D",
+                    f'TYPE="{l[1]}"',
+                    "-D",
+                    f'COLOR="{l[2]}"',
+                    "-D",
+                    f'TEMP_HOTEND="{l[3]}"',
+                    "-D",
+                    f'TEMP_BED="{l[4]}"',
+                    f"{MYDIR}/FilamentSamples.scad",
+                ],
+                check=True,
+            )
+
 
 if __name__ == "__main__":
     gen_samples()
